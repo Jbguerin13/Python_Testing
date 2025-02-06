7b2b22d15d2899123e094a1a3757db9461d2775e
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2025-02-06 17:15:00 +0100

    feat: test login

---
+++ tests/test_show_summary.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_welcome_page_when_loading_accepted(client, setup_data):
+    """TEST: The welcome page should load when credentials are correct"""
+    response = client.post('/show_summary', data={'email': 'clubA@example.com'})
+
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+    assert b"Competition 1" in response.data
+
+def test_error_when_wrong_credentials(client, setup_data):
+    """TEST: Wrong credentials should redirect to index with an error message"""
+    response = client.post('/show_summary', data={'email': 'fake@example.com'}, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"Wrong credentials, please retry" in response.data
