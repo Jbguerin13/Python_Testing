e4e52b5e85a99f412e8c9e21dfc5b99a1b966ded
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2025-02-12 23:34:32 +0100

    fix: test_purchase_place

--- tests/test_purchase_place.py
+++ tests/test_purchase_place.py
@@ -9,7 +9,6 @@ def test_purchase_places_success(client, setup_data):
         'places': '5'
     })
     
-    print(f"toto {response.data}")
     assert response.status_code == 302
     assert int(competitions[0]['numberOfPlaces']) == 15
 
@@ -31,12 +30,14 @@ def test_purchase_places_more_than_12(client, setup_data):
     competitions, _ = setup_data
     response = client.post('/purchasePlaces', data={
         'competition': 'Competition 1',
-        'club': 'Club A',
-        'places': '13'
+        'club': 'Club C',
+        'places': '15'
     })
     
     assert response.status_code == 400
     assert int(competitions[0]['numberOfPlaces']) == 20
+    assert b"You cannot book more than 12 places per competition." in response.data
+
 
 def test_purchase_places_not_enough_points(client, setup_data):
     # TEST cant buy if not enough points
@@ -46,6 +47,7 @@ def test_purchase_places_not_enough_points(client, setup_data):
         'club': 'Club B',
         'places': '5'
     })
-    
+        
     assert response.status_code == 400
     assert int(clubs[1]['points']) == 3
+    assert b"Not enough points available." in response.data
